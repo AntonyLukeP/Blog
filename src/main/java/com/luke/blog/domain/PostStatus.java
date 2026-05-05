df07a701f4566952d7b9d8b2a72a5f85bd325494
Antony Luke <antonyluke001@gmail.com>
2026-05-05 17:55:50 +0530

    added entities

---
+++ src/main/java/com/luke/blog/domain/PostStatus.java
@@ -0,0 +1,5 @@
+package com.luke.blog.domain;
+
+public enum PostStatus {
+    DRAFT, PUBLISHED
+}
--- src/main/java/com/luke/blog/domain/entity/User.java
+++ src/main/java/com/luke/blog/domain/entity/User.java
@@ -42,4 +42,9 @@ public class User {
     public int hashCode() {
         return Objects.hash(id, email, password, name, createdAt);
     }
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
