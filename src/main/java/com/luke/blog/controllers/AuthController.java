9058ab1933bec7f5f6d7ebf1fde52a3640654716
Antony Luke <antonyluke001@gmail.com>
2026-05-10 19:08:39 +0530

    jwt auth

---
+++ src/main/java/com/luke/blog/controllers/AuthController.java
@@ -0,0 +1,35 @@
+package com.luke.blog.controllers;
+
+import com.luke.blog.domain.dtos.AuthResponse;
+import com.luke.blog.domain.dtos.LoginRequest;
+import com.luke.blog.services.AuthenticaionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticaionService authenticaionService;
+
+    @PostMapping
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+        UserDetails userDetails = authenticaionService.authenticate(
+                loginRequest.getEmail(),
+                loginRequest.getPassword()
+        );
+        String tokenValue = authenticaionService.generateToken(userDetails);
+        AuthResponse authResponse = AuthResponse.builder()
+                .token(tokenValue)
+                .expiresIn(86400)
+                .build();
+        return ResponseEntity.ok(authResponse);
+    }
+
+}
--- src/main/java/com/luke/blog/services/AuthenticaionService.java
+++ src/main/java/com/luke/blog/services/AuthenticaionService.java
@@ -1,4 +1,10 @@
 package com.luke.blog.services;
 
+import com.luke.blog.domain.dtos.LoginRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface AuthenticaionService {
+    UserDetails authenticate(String email, String password);
+    String generateToken(UserDetails userDetails);
+    UserDetails validateToken(String token);
 }
