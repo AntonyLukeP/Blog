adba518e2175f47598b65d14115f344cd44c2eea
Antony Luke <antonyluke001@gmail.com>
2026-05-13 10:37:59 +0530

    getPosts endpoint implementation

--- src/main/java/com/luke/blog/controllers/PostController.java
+++ src/main/java/com/luke/blog/controllers/PostController.java
@@ -1,6 +1,9 @@
 package com.luke.blog.controllers;
 
 import com.luke.blog.domain.dtos.PostDto;
+import com.luke.blog.domain.entity.Post;
+import com.luke.blog.mappers.PostMapper;
+import com.luke.blog.services.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,9 +20,16 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class PostController {
 
+    private final PostService postService;
+    private final PostMapper postMapper;
+
     @GetMapping
     public ResponseEntity<List<PostDto>> getAllPosts(
             @RequestParam(required = false)UUID categoryId,
             @RequestParam(required = false)UUID tagId
-            ) {}
+            ) {
+        List<Post> posts = postService.getAllPosts(categoryId, tagId);
+        List<PostDto> postDtos = posts.stream().map(postMapper::toDto).toList();
+        return ResponseEntity.ok(postDtos);
+    }
 }
